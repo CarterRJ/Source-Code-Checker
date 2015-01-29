628c026d6a8353e5e772df1378e268ce0fc2b00f
Ryan Carter <carterrjj@outlook.com>
2015-01-29 16:55:24 +0000

    Implemented the config file 2 rules, renamed (deleted) mispelled congif file

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("MAX_LINE_LENGTH", 89);
+define("MAX_NUM_LINES", 120);
+
+
+?>
--- rules.php
+++ rules.php
@@ -1,18 +1,19 @@
 <?php
+	include("config.php");
 //Error Checking
 echo "<h2> Errors </h2>";
 
 //L006 Source file sould not be too long
 $lines = file($target_file); 
 $numLines = count($lines);
-if($numLines > 130){
-echo "<p>There are <strong>$numLines </strong>lines. Limit is 130</p>";
+if($numLines > MAX_NUM_LINES){
+echo "<p>There are <strong>$numLines </strong>lines. Limit is <strong>".MAX_NUM_LINES."</strong></p>";
 }
 
 //L006 Line should not be too long
 foreach($lines as $line){
-if(strlen($line) > 90){
-	echo "<p>".$line." <strong>line too long maxlength = 90</strong></p>";
+if(strlen($line) > MAX_LINE_LENGTH){
+	echo "<p>".$line." <strong>line too long maxlength = " .MAX_LINE_LENGTH." </strong></p>";
 	}
 } 
 
