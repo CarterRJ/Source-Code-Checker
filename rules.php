226caaa92c6f9bfd9fc3e7776db2b0e2d8d1ee3a
Ryan Carter <carterrjj@outlook.com>
2015-01-30 15:13:17 +0000

    L002 Don't use tab characters

--- rules.php
+++ rules.php
@@ -1,34 +1,42 @@
 <?php
-include("config.php");
-//Error Checking
+include ("config.php");
+// Error Checking
 echo "<h2> Errors </h2>";
 
-//L006 Source file should not be too long
-$lines = file($target_file); 
-$numLines = count($lines);
-if($numLines > MAX_NUM_LINES){
-echo "<p>There are <strong>$numLines </strong>lines. The limit is <strong>".MAX_NUM_LINES."</strong></p>";
+// L006 Source file should not be too long
+$lines = file ( $target_file );
+$numLines = count ( $lines );
+if ($numLines > MAX_NUM_LINES) {
+	echo "<p>There are <strong>$numLines </strong>lines. The limit is <strong>" . MAX_NUM_LINES . "</strong></p>";
 }
 
-//L006 Line should not be too long
-foreach($lines as $line){
-if(strlen($line) > MAX_LINE_LENGTH){
-	echo "<p>".$line." <strong>line too long max line length = " .MAX_LINE_LENGTH." </strong></p>";
+// L006 Line should not be too long
+foreach ( $lines as $line ) {
+	if (strlen ( $line ) > MAX_LINE_LENGTH) {
+		echo "<p>" . $line . " <strong>line too long max line length = " . MAX_LINE_LENGTH . " </strong></p>";
 	}
-} 
-
-//F002 File names should not be too long F002 *PERSONAL VERSION
-if (strlen(basename($target_file)) > MAX_FILENAME_LENGTH){
-	echo"<p> File name <strong>".basename($target_file)." </strong>too long <strong> max length = ".MAX_FILENAME_LENGTH."</strong></p>";
 }
 
-//F001 Source files should not use the '\r' (CR) character FAILED
-$count =0;
-foreach($lines as $line){
-$count++;
-echo "<p>Source files should not use the '\\r' (CR) character. <strong>Line: ".$count."</strong></p>";
+// F002 File names should not be too long F002 *PERSONAL VERSION
+if (strlen ( basename ( $target_file ) ) > MAX_FILENAME_LENGTH) {
+	echo "<p> File name <strong>" . basename ( $target_file ) . " </strong>too long <strong> max length = " . MAX_FILENAME_LENGTH . "</strong></p>";
 }
-	
+
+// F001 Source files should not use the '\r' (CR) character FAILED
+$count = 0;
+foreach ( $lines as $line ) {
+	$count ++;
+	// echo "<p>Source files should not use the '\\r' (CR) character. <strong>Line: ".$count."</strong></p>";
+}
+
+// L002 Don't use tab characters
+$count = 0;
+foreach ( $lines as $line ) {
+	$count ++;
+	if (preg_match ( "/\t.*/", "$line" )) {
+		echo "<p>Source files should not use tabs <strong>Line: " . $count . "</strong></p>";
+	}
+}
 ?>
 	
 	
