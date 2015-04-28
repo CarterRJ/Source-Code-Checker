10e0266fdfa75211c723ba479bec8c11835d1cf1
Ryan Carter <carterrjj@outlook.com>
2015-04-28 17:29:06 +0100

    Parameterised registering Bcrypt passwords First name and last name registering

--- login/index.php
+++ login/index.php
@@ -6,65 +6,60 @@ include '../header.php';
 
 if (isset ( $_SESSION ['LoggedIn'] ) && isset ( $_SESSION ['Username'] )) // If user is already logged in
 {
-	
-	?>
- <meta http-equiv="refresh" content="0;members.php">
-    
-      
-     <?php
+	echo '<meta http-equiv="refresh" content="0;members.php">';
 } elseif (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] )) // If user has submitted username and password
 {
-	$username = mysqli_escape_string ( $db_conn, $_POST ['username'] );
-	$password = md5 ( mysqli_escape_string ( $db_conn, $_POST ['password'] ) );
-	
-	$checklogin = mysqli_query ( $db_conn, "SELECT * FROM users WHERE Username LIKE BINARY '" . $username . "' AND Password LIKE BINARY '" . $password . "'" );
-	if (mysqli_num_rows ( $checklogin ) == 1) {
-		$row = mysqli_fetch_array ( $checklogin );
-		$email = $row ['Email'];
-		$admin = $row ['Admin'];
-		
-		$_SESSION ['Username'] = $username;
-		$_SESSION ['Email'] = $email;
-		$_SESSION ['LoggedIn'] = 1;
-		$_SESSION ['Admin'] = $admin;
-		
-		//echo "<h1>Success</h1>";
-		//echo "<p>We are now redirecting you to the member area.</p>";
-		
-		if($_SESSION['Admin'] != 0){
-			echo "<meta http-equiv='refresh' content='0;admin.php' />";
-		}else{
-		echo "<meta http-equiv='refresh' content='0;index.php' />"; // Meta refresh
-		}
-	} else // Login failed.
-{
-		echo "<h1>Error</h1>";
-		echo "<p>Sorry, your account could not be found. Please <a href=\"index.php\">click here to try again</a>.</p>";
+	$username = $_POST ['username'];
+	if ($checklogin = $db_conn->prepare ( "SELECT * FROM users WHERE Username LIKE BINARY ?" )) {
+		$checklogin->bind_param ( 's', $username );
+		$checklogin->execute ();
+		$result = $checklogin->get_result ();
+		$checklogin->close ();
 	}
-} else // User has not logged in
-{
-	?>		
-		<h1>Member Login</h1>
+	if ($result->num_rows == 1) {
+		$row = $result->fetch_assoc ();
+		
+		if (password_verify ( $_POST ['password'], $row ['Password'] )) {
+			$_SESSION ['Username'] = $username;
+			$_SESSION ['Email'] = $row ['Email'];
+			$_SESSION ['LoggedIn'] = 1;
+			$_SESSION ['Admin'] = $row ['Admin'];
+			
+			if ($_SESSION ['Admin'] != 0) {
+				echo "<meta http-equiv='refresh' content='0;admin.php' />";
+			} else {
+				echo "<meta http-equiv='refresh' content='0;index.php' />"; // Meta refresh
+			}
+			exit();
+		}
+	}
+	
+	echo "<h1>Error</h1>";
+	echo "<p>Sorry, your log in was unsuccessful. Please <a href=\"index.php\">click here to try again</a>.</p>";
+	
+}else{	
+	
+	?>
+<h1>Member Login</h1>
 
-			<p>
-				Thanks for visiting! Please either login below, or <a
-					href="register.php">click here to register</a>.
-			</p>
+<p>
+	Thanks for visiting! Please either login below, or <a
+		href="register.php">click here to register</a>.
+</p>
 
-			<form method="post" action="index.php" name="loginform"
-				id="loginform">
-				<fieldset>
-					<label for="username">Username:</label><input type="text"
-						name="username" id="username" /><br /> <label for="password">Password:</label><input
-						type="password" name="password" id="password" /><br /> <input
-						type="submit" name="login" id="login" value="Login" />
-				</fieldset>
-			</form>
-     
-   <?php
+<form method="post" action="index.php" name="loginform" id="loginform">
+	<fieldset>
+		<label for="username">Username:</label><input type="text"
+			name="username" id="username" /><br /> <label for="password">Password:</label><input
+			type="password" name="password" id="password" /><br /> <input
+			type="submit" name="login" id="login" value="Login" />
+	</fieldset>
+</form>
+
+<?php
 }
 ?>
-	
-	</div>
+
+</div>
 </body>
 </html>
--- login/register.php
+++ login/register.php
@@ -1,55 +1,78 @@
-<?php include "db-info.php";
+<?php
+
+include "db-info.php";
+include '../css/css.php';
+include '../js/js.php';
 include_once '../header.php';
 
-if(!empty($_POST['username']) && !empty($_POST['password']))
-{
-    $username = mysqli_escape_string($db_conn, $_POST['username']);
-    $password = md5(mysqli_escape_string($db_conn, $_POST['password']));
-    $email = mysqli_escape_string($db_conn, $_POST['email']);
-     
-     $checkusername = mysqli_query($db_conn, "SELECT * FROM users WHERE Username = '".$username."'");
-      
-     if(mysqli_num_rows($checkusername) == 1)
-     {
-        echo "<h1>Error</h1>";
-        echo '<p>Sorry, that username is taken. Please go back and <a href="register.php">try again</a>.</p>';
-     }
-     else
-     {
-        $registerquery = mysqli_query($db_conn, "INSERT INTO users (Username, Password, Email) VALUES('".$username."', '".$password."', '".$email."')");
-        if($registerquery)
-        {
-            echo "<h1>Success</h1>";
-            echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
-        }
-        else
-        {
-            echo "<h1>Error</h1>";
-            echo "<p>Sorry, your registration failed. Please go back and <a href=\"register.php\">try again</a>.</p>";    
-        }       
-     }
-}
-else
-{
-    ?>
-     
-   <h1>Register</h1>
-     
-   <p>Please enter your details below to register.</p>
-     
-    <form method="post" action="register.php" name="registerform" id="registerform">
-    <fieldset>
-        <label for="username">Username:</label><input type="text" name="username" id="username" /><br />
-        <label for="password">Password:</label><input type="password" name="password" id="password" /><br />
-        <label for="email">Email Address:</label><input type="text" name="email" id="email" /><br />
-        <input type="submit" name="register" id="register" value="Register" />
-    </fieldset>
-    </form>
-     
-    <?php
+var_dump($_POST);
+
+if (! empty ( $_POST ['username'] ) && ! empty ( $_POST ['password'] )) {
+	
+	$username = $_POST ['username'];
+	$password = password_hash ( $_POST ['password'], PASSWORD_BCRYPT );
+	$fname = $_POST ['fname'];
+	$lname = $_POST ['lname'];
+	
+	$email = $_POST ['email'];
+	if ($checkusername = $db_conn->prepare ( "SELECT * FROM users WHERE Username =?" )) {
+		$checkusername->bind_param ( 's', $username );
+		$checkusername->execute ();
+		$checkusername->store_result ();
+	}
+	
+	if ($checkusername->num_rows == 1) {
+		echo "<h1>Error</h1>";
+		echo '<p>Sorry, that username is taken. Please go back and <a href="register.php">try again</a>.</p>';
+		$checkusername->close ();
+	} else {
+		$checkusername->close ();
+		if ($registerquery = $db_conn->prepare ( "INSERT INTO users (Username, Password, fName, lName, Email) VALUES(?, ?, ?, ?, ?)" )) {
+			$registerquery->bind_param ( 'sss', $username, $password, $fname, $lname, $email );
+			$registerquery->execute ();
+			$registerquery->close ();
+			echo "<h1>Success</h1>";
+			echo "<p>Your account was successfully created. Please <a href=\"index.php\">click here to login</a>.</p>";
+		} else {
+			echo "<h1>Error</h1>";
+			echo "<p>Sorry, your registration failed. Please go back and <a href=\"register.php\">try again</a>.</p>";
+		}
+	}
+} else {
+	?>
+
+<h1>Register</h1>
+
+<p>Please enter your details below to register.</p>
+
+<form class="form-horizontal" method="post" action="register.php" name="registerform"
+	id="registerform">
+	<fieldset>
+		<label for="username">Username:</label>
+		<input type="text" name="username" id="username" /><br />
+		
+		<label for="password">Password:</label>
+		<input type="password" name="password" id="password" /><br />
+		
+		
+		<label for="fname">First Name:</label>
+		<input type="text" name="fname" id="fname" /><br />
+		
+		
+		<label for="lname">Last Name:</label>
+		<input type="text" name="lname" id="lname" /><br />
+		
+		<label for="email">Email Address:</label>
+		<input type="text" name="email" id="email" /><br />
+		
+		<input type="submit" name="register" id="register" value="Register" />
+	</fieldset>
+</form>
+
+<?php
 }
 ?>
- 
+
 </div>
 </body>
 </html>
